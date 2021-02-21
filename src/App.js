4ce5c99fa900e5bc29f574d2943181121dfb0280
JingyiNiu <niu.jy1203@gmail.com>
2021-02-21 16:38:52 +1300

    implement reach router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Router } from "@reach/router";
+
 import Nav from './components/nav/nav.component';
 import Footer from './components/footer/footer.component';
+
+import Homepage from './pages/homepage/homepage.jsx';
+import Blog from './pages/blog/blog.jsx';
 
 import './App.css';
 
@@ -8,6 +13,10 @@ function App() {
   return (
     <div className="app-container">
       <Nav />
+      <Router>
+        <Homepage path="/" />
+        <Blog path="blog" />
+      </Router>
       <Footer/>
     </div>
   );
--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Menu } from 'antd';
 import { HomeOutlined, BookOutlined, FormOutlined } from '@ant-design/icons';
+import { Link } from "@reach/router";
+
 import './nav.style.css';
 
 function Nav() {
     return (
         <Menu mode="horizontal" className='nav-container' theme="dark">
           <Menu.Item key="home" icon={<HomeOutlined />}>
-            Home
+            <Link to="/">Home</Link>
           </Menu.Item>
           <Menu.Item key="blog" icon={<BookOutlined />}>
-            Blog
+            <Link to="blog">Blog</Link>
           </Menu.Item>
           <Menu.Item key="new-post" icon={<FormOutlined />}>
             Create New Post
