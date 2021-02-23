9669f2b5b3aa431b2cc0c43628b2236c4fc2a2e5
JingyiNiu <niu.jy1203@gmail.com>
2021-02-24 12:23:24 +1300

    insert data into firestore remove yarn

--- src/components/new-post/new-post.component.jsx
+++ src/components/new-post/new-post.component.jsx
@@ -13,7 +13,17 @@ const NewPost = (props) => {
     const onContentChange = (event) => setContent(event.target.value);
 
     const onCreatPost = () => {
-        console.log("Post Created")
+        let postRef = db.collection('posts');
+        let payload = {title, content};
+
+        postRef.add(payload)
+            .then((docRef) => {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });
+
     }
 
 
