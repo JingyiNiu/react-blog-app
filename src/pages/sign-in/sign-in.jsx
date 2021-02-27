4d2a1129eb40aab82a730892a929faf1df6f3667
JingyiNiu <niu.jy1203@gmail.com>
2021-02-27 16:21:18 +1300

    implememt sign in

--- src/pages/sign-in/sign-in.jsx
+++ src/pages/sign-in/sign-in.jsx
@@ -7,6 +7,27 @@ import { auth } from '../../firebase';
 import './sign-in.style.css';
 
 const SignIn = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onEmailChange = (event) => setEmail(event.target.value);
+    const onPasswordChange = (event) => setPassword(event.target.value);
+
+    const onSignin = () => {
+        auth.signInWithEmailAndPassword(email, password)
+            .then((user) => {
+                setEmail('');
+                setPassword('');
+                navigate('../');
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+    }
+
     return (
         <div className='signin-container'>
             <div className='page-header'>
@@ -17,14 +38,14 @@ const SignIn = () => {
             
             <div className='signin-inputs-container'>
                 <div className='signin-item-container'>
-                    <Input size="large" placeholder="Email" prefix={<MailOutlined />} />
+                    <Input size="large" placeholder="Email" prefix={<MailOutlined />} onChange={onEmailChange} />
                 </div>
                 <div className='signin-item-container'>
-                    <Input.Password size="large" placeholder="Password" prefix={<LockOutlined />} />
+                    <Input.Password size="large" placeholder="Password" prefix={<LockOutlined />}  onChange={onPasswordChange} />
                 </div>
                 <div className='signin-item-container'>
                 <Link to="../register">Don't have an account, Register</Link>
-                    <Button type="primary" size="large" style={{float:"right"}}>Sign In</Button>
+                    <Button type="primary" size="large" style={{float:"right"}}  onClick={onSignin} >Sign In</Button>
                 </div>
             </div>
         </div>
